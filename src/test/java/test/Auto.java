13d39e3ef9263d2be3264ffac11ed9ee475af86c
tmurilloa <tmurilloa@unal.edu.co>
2023-08-20 17:22:37 -0500

    clases Asiento y Motor
    ya hice las clases de Asiento y Motor solo faltan los metodos de Auto

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	String marca;
+	Motor motor;
+	int registro;
+	public static int cantidadCreados;
+	Asiento [] asientos;
+	
+	int cantidadAsientos() {
+		
+	}
+	
+	String verificarIntegridad() {
+		
+	}
+}
---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,27 @@
+package test;
+
+public class Motor {
+	
+	int numeroCilindros;
+	String tipo;
+	int registro;
+	
+	void cambiarRegistro(int registro) 
+	{
+		this.registro = registro;
+		
+	}
+	
+	void asignarTipo(String tipo) 
+	{
+		if (tipo == "electrico" || tipo == "gasolina")
+		{
+			this.tipo = tipo;
+			
+		}
+		
+	}
+	
+	
+
+}
