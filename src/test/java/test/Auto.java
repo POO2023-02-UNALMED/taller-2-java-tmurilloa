f4879699a28013ece8ae2b4663996360b1eb2443
tmurilloa <tmurilloa@unal.edu.co>
2023-08-20 18:03:05 -0500

    actualizacion metodos Auto

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -10,10 +10,36 @@ public class Auto {
 	Asiento [] asientos;
 	
 	int cantidadAsientos() {
+		int cantAsientos = 0;
+		for (int i = 0; i < asientos.length; ++i ) {
+			
+			if (asientos[i] != null) {
+				
+				cantAsientos++;
+				
+			}
+		}
+		return cantAsientos;
 		
 	}
 	
 	String verificarIntegridad() {
+		int autoRegistro = this.registro;
+		String original = "Auto original";
+		String pirata = "Las piezas no son originales";
+		if (motor.registro != autoRegistro) {
+			
+			return pirata;
 		
+		}
+		for (int i = 0; i < asientos.length; ++i ) {
+			
+			if (asientos[i].registro != autoRegistro) {
+				
+				return pirata;
+		
+			}
+		}
+		return original;
 	}
 }
